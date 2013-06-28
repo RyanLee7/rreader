cb690300f4cbacbe09ec4c364eb3552bb44cc9c1
yanzhenjiang <836045106@qq.com>
2013-06-28 10:02:55 +0800

    子目录翻译成话题

--- p/src/com/ryan/redreader/common/Globals.java
+++ p/src/com/ryan/redreader/common/Globals.java
@@ -4,20 +4,22 @@ import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 
+/**
+ * 
+ * @author ryanlee
+ *
+ */
 public class Globals {
 
-	public static final boolean AD_MODE = true;// �Ƿ������
-	public static boolean NETWORK_ENABLE = false;// ����״̬
-
-	public static final String AD_KEY = "0npyuclq78s818";// ���˹��KEY
+	public static final boolean AD_MODE = true;// 广告模式
+	public static boolean NETWORK_ENABLE = false;// 
+	public static final String AD_KEY = "0npyuclq78s818";// 
 
 	public static boolean isConnect(Context context) {
-		// ��ȡ�ֻ��������ӹ�����󣨰�����wi-fi,net�����ӵĹ��?
 		try {
 			ConnectivityManager connectivity = (ConnectivityManager) context
 					.getSystemService(Context.CONNECTIVITY_SERVICE);
 			if (connectivity != null) {
-				// ��ȡ�������ӹ���Ķ���
 				NetworkInfo info = connectivity.getActiveNetworkInfo();
 				if (info != null && info.isConnected()) {
 					// �жϵ�ǰ�����Ƿ��Ѿ�����
